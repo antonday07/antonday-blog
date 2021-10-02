af2291a8cf75b695b14ad2121a89f1947f9bf3d5
Antonday07 <anhquangtong2000@gmail.com>
2021-10-02 11:31:10 +0700

    Fix eslint

--- pages/index.js
+++ pages/index.js
@@ -18,13 +18,12 @@ export default function Home({ posts }) {
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="">
-        <div class="pt-6 pb-6">
-            <h1 class="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-              Hi, I’m Quang Anh.
-            </h1>
-          
+        <div className="pt-6 pb-6">
+          <h1 className="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Hi, I’m Quang Anh.
+          </h1>
         </div>
-        
+
         <h2 className="flex pt-6 pb-6 text-2xl font-extrabold text-gray-900 dark:text-gray-100 sm:text-3xl md:text-5xl">
           Latest
         </h2>
